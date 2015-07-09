d5e9db2c02c1e4bfbdb814f049687cbb30ec200d
Lamar Wilson <lamar.wilson@gmail.com>
2015-07-09 06:06:45 -0700

    clean up

--- project/apps/starter/modules/starter/routes.js
+++ project/apps/starter/modules/starter/routes.js
@@ -2,6 +2,5 @@ module.exports = function(app, module) {
   var namespace = module.name;
   var path = "/" + namespace;
   app.$routes[path] = module;
-  console.log(app);
   app.$routes['/todo'] = app.$cmp.list;
 }
