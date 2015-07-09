461c6d6a50441ac3305aa194372c224941908c23
Lamar Wilson <lamar.wilson@gmail.com>
2015-07-09 05:45:19 -0700

    updated hey.js

--- hey.js
+++ hey.js
@@ -6,20 +6,5 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, '/dist')));
 
 http.listen(3400, function() {
-    console.log('listening on *:3400');
+  console.log('listening on *:3400');
 });
-
-
-var restify = require('restify');
-
-var server = restify.createServer({
-  name: 'MyApp',
-});
-
-function send(req, res, next) {
-  res.json({message:'hello ' + req.params.name});
-  return next();
-}
-server.get('/:name', send);
-
-server.listen(8080);
