b4c2a55e75a850d252f51cee3aff564a6f9e77b5
Lamar Wilson <lamar.wilson@gmail.com>
2015-07-18 00:30:26 -0400

    Added config emp back

---
+++ project/config.temp.js
@@ -0,0 +1,9 @@
+window.type = "bctestnet";
+window.appState = "development";
+module.exports = {
+  Parse: {
+    appName: "<app name>",
+    appId: "<appId from Parse>",
+    javascriptKey: "<javascriptKey from Parse>"
+  }
+}
